d288cc9954264a62f1c89d3f424a8bc4a28364fe
Carlos35n4 <31332160+Carlos35n4@users.noreply.github.com>
2019-05-26 00:55:22 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Craft-Helmet</title>
+		<link rel="stylesheet" href="css/mystyle.css">
+	</head>
+	<body>
+		<!--head-->
+		<?php require 'header.php';?>
+		<!--body-->
+		<!--footer-->
+		<?php include 'footer.php';?>
+	</body>
+</html>
